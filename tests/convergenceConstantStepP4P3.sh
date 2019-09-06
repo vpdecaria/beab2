4d588aecd8d1db3a8c3cdfdbdfb1c35ea4e1be5e
vpdecaria <vpd7@pitt.edu>
2019-09-06 16:39:22 -0400

    Added a scott-vogelius test

---
+++ tests/convergenceConstantStepP4P3.sh
@@ -0,0 +1,28 @@
+#Mean to be run from one directory level up.
+let mpi_threads=4
+
+for vo in 1
+do
+	for extrap in 'fe' 'ab2'
+	do
+		for dt in '0.2' '0.1' '0.05' '0.025' '0.0125' '0.00625'
+		do
+			mpirun -np $mpi_threads python3 BEAB2.py --constant -p taylor_green_problemP4P3 -k $dt --error -o errors/convergenceTestConstantStepP4P3/order-$vo-extrap-$extrap-dt-$dt.txt --vo $vo --solver lu --extrap $extrap
+		done
+	done
+
+done
+
+
+for vo in 2
+do
+	for extrap in 'ab2'
+	do
+		for dt in '0.2' '0.1' '0.05' '0.025' '0.0125' '0.00625'
+		do
+			mpirun -np $mpi_threads python3 BEAB2.py --constant -p taylor_green_problemP4P3 -k $dt --error -o errors/convergenceTestConstantStepP4P3/order-$vo-extrap-$extrap-dt-$dt.txt --vo $vo --solver lu --extrap $extrap
+		done
+	done
+
+done
+
